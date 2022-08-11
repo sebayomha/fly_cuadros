14dbbb23d610dd2a2e7a71c4e5f2e0b73bf974ee
Sebastian Yomha <sebastian_yomha@hotmail.com>
2022-08-11 00:17:02 -0300

    feat: se añaden efectos visuales

--- fly_cuadros/src/app/shared/material.module.ts
+++ fly_cuadros/src/app/shared/material.module.ts
@@ -4,10 +4,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card'
+import { MatRippleModule } from '@angular/material/core';
 @NgModule({
   declarations: [
   ],
-  exports:[ MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule ],
+  exports:[ MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatRippleModule ],
   providers: [],
 })
 export class MaterialModule { }
